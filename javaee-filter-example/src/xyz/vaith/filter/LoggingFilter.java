9e3956bc87064144ae4cd3e633582236662accf9
Vaith <vaithwee@yeah.net>
2018-12-03 17:42:09 +0800

    Add filter log example

---
+++ javaee-filter-example/src/xyz/vaith/filter/LoggingFilter.java
@@ -0,0 +1,50 @@
+package xyz.vaith.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebFilter(filterName = "LoggingFilter", urlPatterns = {"/*"}, initParams = {
+        @WebInitParam(name = "logFileName", value = "log.txt"),
+        @WebInitParam(name = "prefix", value = "URI: ")
+})
+public class LoggingFilter implements Filter {
+    private PrintWriter logger;
+    private String prefix;
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("LoggingFilter.doFilter");
+        HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+        logger.println(new Date() + " " + prefix + httpServletRequest.getRequestURI());
+        logger.flush();
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        prefix = filterConfig.getInitParameter("prefix");
+        String logFileName = filterConfig.getInitParameter("logFileName");
+        String appPath = filterConfig.getServletContext().getRealPath("/");
+        System.out.println("logFilePath: " + logFileName);
+        try {
+            logger = new PrintWriter(new File(appPath, logFileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new ServletException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroying filter");
+        if (logger != null) {
+            logger.close();
+        }
+    }
+}
